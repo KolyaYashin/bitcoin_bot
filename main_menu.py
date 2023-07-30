d93c255af9251efcc6a7db335db07bda45ba9308
Yashin Nikolay <kol.yashin@yandex.ru>
2023-07-30 20:54:15 +0300

    old model

--- main_menu.py
+++ main_menu.py
@@ -8,6 +8,9 @@ async def set_main_menu(bot: Bot):
         BotCommand(command='get', description=LEXICON_RU['get_d']),
         BotCommand(command='predict', description=LEXICON_RU['predict_d']),
         BotCommand(command='visualize', description=LEXICON_RU['vis_d']),
+        BotCommand(command='get_old', description=LEXICON_RU['get_d']),
+        BotCommand(command='predict_old', description=LEXICON_RU['predict_d']),
+        BotCommand(command='visualize_old', description=LEXICON_RU['vis_d']),
         BotCommand(command='settings', description=LEXICON_RU['settings_d'])
     ]
 
