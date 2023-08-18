e081f17b4e9324696bf758fbfa0de0a158d4024c
Yashin Nikolay <kol.yashin@yandex.ru>
2023-08-19 01:39:23 +0300

    main_menu

--- main_menu.py
+++ main_menu.py
@@ -4,8 +4,8 @@ from aiogram.types import BotCommand
 
 async def set_main_menu(bot: Bot):
     main_menu_commands=[
-        BotCommand(command='start', description=LEXICON_RU['start_d']),
         BotCommand(command='get', description=LEXICON_RU['get_d']),
+        BotCommand(command='show', description=LEXICON_RU['show']),
         BotCommand(command='predict', description=LEXICON_RU['predict_d']),
         BotCommand(command='visualize', description=LEXICON_RU['vis_d']),
         BotCommand(command='get_old', description=LEXICON_RU['get_d']),
